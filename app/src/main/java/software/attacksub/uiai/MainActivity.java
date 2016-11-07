800409f361bd633cb9387bc7f9b3869a4391d19a
rhyland <rob_hyland@sbcglobal.net>
2016-11-06 19:22:44 -0700

    Remove comment.

--- app/src/main/java/software/attacksub/uiai/MainActivity.java
+++ app/src/main/java/software/attacksub/uiai/MainActivity.java
@@ -3,10 +3,7 @@ package software.attacksub.uiai;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-/*
-    This is a test-commit.
-       And more...
- */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
